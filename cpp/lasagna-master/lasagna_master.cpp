1e5560ba3af0a093dcc1d21b8f267c8fd729238d
kn0rp <170002168+kn0rp@users.noreply.github.com>
2025-11-28 19:49:21 +0100

    wtf is wrong with my lasagna?!

--- cpp/lasagna-master/lasagna_master.cpp
+++ cpp/lasagna-master/lasagna_master.cpp
@@ -9,4 +9,39 @@ int preparationTime(std::vector<std::string> layers,
   return avg_prep_time_per_layer * layers.size();
 }
 
+void addSecretIngredient(std::vector<std::string> &my_recipe_ingredients,
+                         const std::vector<std::string> &friends_ingredients) {
+  my_recipe_ingredients.at(my_recipe_ingredients.size() - 1) =
+      friends_ingredients.at(friends_ingredients.size() - 1);
+}
+
+std::vector<double> scaleRecipe(std::vector<double> portion_amounts,
+                                int portion_number = 2) {
+  std::vector<double> scaled_amounts{};
+  for (double ingredient_amount : portion_amounts) {
+    scaled_amounts.emplace_back(ingredient_amount * (portion_number / 2));
+  }
+  return scaled_amounts;
+}
+
+amount::amount(int noodles, double sauce) {
+  this->noodles = noodles;
+  this->sauce = sauce;
+}
+
+amount quantities(std::vector<std::string> layers) {
+  int noodles_counter{0};
+  double sauce_counter{0};
+
+  for (std::string layer : layers) {
+    if (layer == "noodles") {
+      noodles_counter++;
+    } else if (layer == "sauce") {
+      sauce_counter++;
+    }
+  }
+
+  return amount(noodles_counter * 50, sauce_counter * 0.2);
+};
+
 } // namespace lasagna_master
--- cpp/lasagna-master/lasagna_master.h
+++ cpp/lasagna-master/lasagna_master.h
@@ -1,10 +1,21 @@
 #pragma once
 
+#include <string>
+#include <vector>
 namespace lasagna_master {
 
+int preparationTime(std::vector<std::string>, int);
+
+void addSecretIngredient(std::vector<std::string>, std::vector<std::string>);
+
+std::vector<double> scaleRecipe(std::vector<double>, int);
+
 struct amount {
-    int noodles;
-    double sauce;
+  amount(int noodles, double sauce);
+  int noodles;
+  double sauce;
 };
 
-}  // namespace lasagna_master
+amount quantities(int, double);
+
+} // namespace lasagna_master
