15e3c2363da386fcbed7dc1975263ead33dde0ee
kn0rp <170002168+kn0rp@users.noreply.github.com>
2025-12-12 01:01:18 +0100

    solved grade_school

--- cpp/grade-school/grade_school.cpp
+++ cpp/grade-school/grade_school.cpp
@@ -1,3 +1,33 @@
 #include "grade_school.h"
+#include <algorithm>
+#include <string>
+#include <utility>
+#include <vector>
 
-namespace grade_school {} // namespace grade_school
+namespace grade_school {
+const std::map<int, std::vector<std::string>> &school::roster() const {
+  return school::roster_;
+}
+
+const std::vector<std::string> school::grade(int grade) const {
+  if (school::roster_.find(grade) != school::roster_.end()) {
+    std::vector<std::string> grade_name_list = school::roster_.at(grade);
+    return grade_name_list;
+  } else {
+    return {};
+  }
+}
+
+void school::add(std::string name, int grade) {
+  if (school::roster_.find(grade) != roster_.end()) {
+    std::vector<std::string> &names{school::roster_.at(grade)};
+    if (std::find(names.begin(), names.end(), name) == names.end()) {
+      names.emplace_back(name);
+      std::sort(names.begin(), names.end());
+    }
+  } else {
+    school::roster_.emplace(
+        std::pair<int, std::vector<std::string>>{grade, {name}});
+  }
+}
+} // namespace grade_school
--- cpp/grade-school/grade_school.h
+++ cpp/grade-school/grade_school.h
@@ -1,9 +1,20 @@
 #pragma once
 
+#include <map>
+#include <string>
+#include <vector>
 namespace grade_school {
 
 // TODO: add your solution here
 
-struct school {};
+class school {
+private:
+  std::map<int, std::vector<std::string>> roster_;
 
+public:
+  school() = default;
+  const std::map<int, std::vector<std::string>> &roster() const;
+  const std::vector<std::string> grade(int grade) const;
+  void add(std::string name, int grade);
+};
 } // namespace grade_school
