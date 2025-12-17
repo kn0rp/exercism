bfff31d7f9bd1da3638ee4c2011bda093baf79e3
kn0rp <170002168+kn0rp@users.noreply.github.com>
2025-12-17 18:37:32 +0100

    solved diffOfSquares

--- cpp/difference-of-squares/difference_of_squares.cpp
+++ cpp/difference-of-squares/difference_of_squares.cpp
@@ -1,7 +1,28 @@
 #include "difference_of_squares.h"
+#include <cmath>
 
-namespace difference {
-int difference_of_squares(int num) {
-  return difference_of_squares::square_of_sum(num) - difference_of_squares::sum_of_squares(num);
+namespace difference_of_squares {
+
+int square_of_sum(int num) {
+  int square{0};
+  for (int i{1}; i <= num; i++) {
+    square += i;
+  }
+  return std::pow(square, 2);
 }
-}  // namespace difference_of_squares
+
+int sum_of_squares(int num) {
+  int sum{0};
+  for (int i{1}; i <= num; i++) {
+    sum += std::pow(i, 2);
+  }
+  return sum;
+}
+
+int difference(int num) {
+  return (square_of_sum(num) > sum_of_squares(num))
+             ? square_of_sum(num) - sum_of_squares(num)
+             : sum_of_squares(num) - square_of_sum(num);
+}
+
+} // namespace difference_of_squares
--- cpp/difference-of-squares/difference_of_squares.h
+++ cpp/difference-of-squares/difference_of_squares.h
@@ -4,20 +4,7 @@
 #include <math.h>
 
 namespace difference_of_squares {
-int square_of_sum(int num) {
-  int runner{0};
-  for (int i{1}; i <= num; i++) {
-    runner += i;
-  }
-  return pow(runner, 2);
-}
-
-int sum_of_squares(int num) {
-  int runner{0};
-  for (int i{1}; i <= num; i++) {
-    runner += pow(i, 2);
-  }
-  return runner;
-}
-
+int sum_of_squares(int);
+int square_of_sum(int);
+int difference(int);
 } // namespace difference_of_squares
